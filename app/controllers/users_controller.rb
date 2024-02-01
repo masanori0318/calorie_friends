ba4bead9aaffe7867b7cb45b08739be31c558838
masanori0318 <144527559+masanori0318@users.noreply.github.com>
2024-02-02 01:37:41 +0900

    基礎代謝計算結果

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,16 @@ class UsersController < ApplicationController
     
   end
 
-  def edit
+  def new
+    @user = User.new
   end
 
+  def edit
+  end
+  def show
+    user = User.find(params[:id])
+    @name = current_user.name
+    @users = current_user.users
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
   resources :users, only: [:new, :create, :edit, :update]
-  #root 'users#index'
-  #post '/calculations/calculate_bmr', to: 'calculations#calculate_bmr'
+  post '/calculations/calculate_bmr', to: 'calculations#calculate_bmr'
 end
