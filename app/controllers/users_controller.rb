f12d0320852c90dd5aaa1dd18b8f37176aa0c0f1
masanori0318 <144527559+masanori0318@users.noreply.github.com>
2024-02-02 00:32:03 +0900

    ログイン・ログアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,20 +7,5 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def calculate_bmr
-    weight = params[:weight].to_f
-    height = params[:height].to_f
-    age = params[:age].to_i
-    gender = params[:gender]
-    
-    if gender == 'male'
-      bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
-    elsif gender == 'female'
-      bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
-    else
-      bmr = 0
-    end
-
-    render json: { bmr: bmr }
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
-  resources :users, only: [:edit, :update]
-  #root 'calculations#index'
+  resources :users, only: [:new, :create, :edit, :update]
+  #root 'users#index'
   #post '/calculations/calculate_bmr', to: 'calculations#calculate_bmr'
 end
