dfb79a895fc268ba1979ffd7eef77dded412f768
masanori0318 <144527559+masanori0318@users.noreply.github.com>
2024-02-02 11:36:38 +0900

    マイページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :edit, :update]
+ 
   def index
     
   end
 
   def new
-    @user = User.new
+    
   end
 
   def edit
   end
   def show
-    user = User.find(params[:id])
-    @name = current_user.name
-    @users = current_user.users
+    @user = User.find(params[:id])
+    #@name = current_user.name
+    #@users = current_user.users
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
-  resources :users, only: [:new, :create, :edit, :update]
+  resources :users, only: [:new, :create, :edit, :update, :show]
   post '/calculations/calculate_bmr', to: 'calculations#calculate_bmr'
 end
