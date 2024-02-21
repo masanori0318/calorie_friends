b486b8de56f579b6eb132839006f7d66f8d210a5
masanori0318 <144527559+masanori0318@users.noreply.github.com>
2024-02-21 10:02:04 +0900

    編集機能・削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
-  resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
   resources :meals, only: [:index, :create]
   resources :calories, only: [:index, :create]
-  resources :records
+  resources :records, only: [:index, :new, :create, :show, :edit, :update, :destroy] # recordsにcreateアクションのみを追加
   post '/calculations/calculate_bmr', to: 'calculations#calculate_bmr'
   get 'home', to: 'records#home'
   get 'day_record', to: 'records#day_record'
