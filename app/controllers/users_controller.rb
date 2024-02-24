bfd7290cd2c0ee32e6a3303d3d72ebe9a7c2b45e
masanori0318 <144527559+masanori0318@users.noreply.github.com>
2024-02-25 01:12:07 +0900

    アカウント編集可

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,14 @@ class UsersController < ApplicationController
 
   def edit
     @user = current_user
+    @confirmable_enabled = User.devise_modules.include?(:confirmable)
     set_minimum_password_length
   end
 
   def show
     @user = User.find(params[:id])
+  end
+  def set_minimum_password_length
+    @minimum_password_length = User.password_length.min
   end
 end
