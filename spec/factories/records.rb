d0da19ec49a17e33e0466022bb95f7c5247eb162
masanori0318 <144527559+masanori0318@users.noreply.github.com>
2024-03-04 21:49:51 +0900

    テストコード

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :record do
-    
+    user
+    breakfast { "Some value" }
+    lunch { "Some lunch value" }
+    dinner { "Some dinner value" }
+    snack { "Some snack value" }
+    breakfast_cal { 300 }  # breakfast_cal 列に適切な値を設定する
+    lunch_cal { 400 }
+    dinner_cal { 500 }
+    snack_cal { 200 }
   end
 end
--- spec/requests/records_spec.rb
+++ spec/requests/records_spec.rb
@@ -10,11 +10,16 @@ RSpec.describe RecordsController, type: :request do
   end
 
   describe "GET #index" do
-    it "正常なレスポンスを返すこと" do
+    it "indexアクションに正常なレスポンスを返すこと" do
       get records_path
       expect(response.status).to eq 200
     end
-
     
+    it "indexアクションにリクエストするとレスポンスに投稿済みのレコード情報が存在する" do
+      record = FactoryBot.create(:record, user: user, breakfast: "Some value", lunch: "Some lunch value", dinner: "Some dinner value", snack: "Some snack value", breakfast_cal: 100, lunch_cal: 400, dinner_cal: 500, snack_cal: 200)
+      puts "Generated record breakfast: #{record.breakfast}"
+      get records_path
+      expect(response.body).to include(record.breakfast)
+    end
   end
 end
