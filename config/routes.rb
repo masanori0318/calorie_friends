847a6ee21e963a055a50d8c2a9677b4cecb5a53a
masanori0318 <144527559+masanori0318@users.noreply.github.com>
2024-03-11 13:34:54 +0900

    render

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post '/calculations/calculate_bmr', to: 'calculations#calculate_bmr'
   get 'home', to: 'records#home'
   get 'day_record/:year/:month/:day', to: 'records#day_record', as: 'day_record'
-
+  
   devise_scope :user do
     get 'users/password/edit_custom', to: 'devise/passwords#edit', as: :edit_user_password_custom
     put 'users/password/update_custom', to: 'devise/passwords#update', as: :user_password_custom
